30ad5b0d275c4bb5fdaf29f6c6d97be1f1bc0a3d
ooreally <anu1512sri@gmail.com>
2021-06-12 00:13:37 +0530

    refactor: style

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import authReducer from './store/reducers/auth';
 
-const composedEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composedEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer
 });
-const store = createStore(rootReducer, composedEnhancers(
+const store = createStore(rootReducer, 
   applyMiddleware(thunk)
-));
+);
 
 
 
